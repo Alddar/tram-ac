445d61f3349d7eb2260a8abe85607f7768fe50b4
Ondrej Zavodny <ozavodny@purestorage.com>
2023-07-12 12:52:09 +0200

    Hello

---
+++ app/reload.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export function Reload() {
+
+  const router = useRouter()
+  useEffect(() => {
+    const interval = setInterval(() => {
+      router.refresh()
+    }, 60000)
+
+    return () => clearInterval(interval)
+  }, [router])
+    
+
+  return <></>
+}
