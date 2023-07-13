f1943ee27e81303eb1e4e42a2d43d04ee6ebd018
Ondřej Závodný <ondrej@zavodny.net>
2023-07-13 02:07:26 +0200

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,48 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Prague is too hot!',
-  description: 'Find air conditioned public transport in Prague.',
-}
+  title: "Prague is too hot!",
+  description: "Find air conditioned public transport in Prague.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>{children}</body>
-    </html>
-  )
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <html lang="en">
+        <body className={`${inter.className} bg-black text-white`}>
+          {children}
+        </body>
+      </html>
+    </>
+  );
 }
